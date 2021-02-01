91c2d2b581a1639307aaa5489848f224e96e9c4e
jawnhoang <johnhoang5@gmail.com>
2021-02-01 00:16:44 -0800

    latested website version

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+function archerproj(){
+  var dirPath = dirname(location.href);
+  fullPath = dirPath + "/archer.html";
+  window.location = fullPath;
+}
+
+function jinaproj(){
+  var dirPath = dirname(location.href);
+  fullPath = dirPath + "/jina.html";
+  window.location = fullPath;
+}
 
 function home(){
     var dirPath = dirname(location.href);
@@ -20,17 +31,31 @@ function dirname(path){
     return path.replace(/\\/g,'/').replace(/\/[^\/]*$/,'');
 }
 
-var myIndex = 0;
-carousel();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function carousel() {
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 3000);
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
